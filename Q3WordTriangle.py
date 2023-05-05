e5741e999c3ce1562ca292fca63cf3dd145942ba
Amelia Mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-05-05 15:44:11 +0000

    fix

--- Q3WordTriangle.py
+++ Q3WordTriangle.py
@@ -1,7 +1,14 @@
-# Create a program that will prompt the user for a word, and return a 'word triangle'. For example, if the user types in the word "PYTHON", your program will output:
+# Create a program that will prompt the user for a word, and return a 'word triangle'. 
+# For example, if the user types in the word "PYTHON", your program will output:
 # P
 # PY
 # PYT
 # PYTH
 # PYTHO
 # PYTHON
+
+word = str(input("Please enter a word: "))
+for r in range(len(word)):
+    for c in range(r + 1):
+        print(word[c], end=' ')
+    print()
